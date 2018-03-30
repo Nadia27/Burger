33633a26bb0d523918c1aff71eb2a58dfa3d81aa
Nadia27 <jones.nadia.l@gmail.com>
2018-03-30 10:56:11 -0400

    unfinished

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers VALUES(1, "Bacon Cheeseburger", false);
+INSERT INTO burgers VALUES(2, "Grilled Onions and House Pickles", false);
+INSERT INTO burgers VALUES(3, "Guacamole Bacon Burger", false);

645e5f48dc8fec73330724fccddca00818cef33c
Nadia27 <jones.nadia.l@gmail.com>
2018-04-04 14:16:55 -0400

    heroku setup

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.use(methodOverride('_method'))
+app.use(methodOverride('_method'));
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
